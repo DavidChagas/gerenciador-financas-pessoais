f62beea18af2293273cfc2cc2b34c048a9740c3c
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2021-09-02 20:28:02 -0300

    limpando projeto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,10 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                @endif
                             </div>
                         </div>
                     </form>
